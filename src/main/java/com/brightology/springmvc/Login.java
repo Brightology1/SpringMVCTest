0f55386e24a09691f2f2f8e8da5e474fd15ee4a7
Brightology1 <bright.anokwuru@decagon.dev>
2022-05-19 22:52:10 +0100

    Initial Commit

---
+++ src/main/java/com/brightology/springmvc/Login.java
@@ -0,0 +1,11 @@
+package com.brightology.springmvc;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Login {
+    public String login(){
+        return "Nothing, just test";
+    }
+
+}
